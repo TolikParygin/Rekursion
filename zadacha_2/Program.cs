017d4eb7832d3864c8c27a9801481ed9d44c185b
Tolik <anatolijparygin988@gmail.com>
2022-12-22 00:28:52 +0300

    сумма цифр от M до N

--- zadacha_2/Program.cs
+++ zadacha_2/Program.cs
@@ -5,13 +5,17 @@
 System.Console.Write("Веедите число N: ");
 int number = int.Parse(Console.ReadLine());
 
- int Sum(int num)
+int Sum(int num)
 {
-    if (num < 10) 
-    return num;
-    else 
-  return num % 10 + Sum(num / 10);
-    
+    if (num < 10)
+    {
+        return num;
+    }
+    else
+    {
+        return num % 10 + Sum(num / 10);
+    }
+
 }
 int sum = Sum(number);
 System.Console.WriteLine(sum);
---
+++ zadacha_3/Program.cs
@@ -0,0 +1,15 @@
+﻿// сумма цифр от N до M
+
+void Sum(int n, int m)
+{
+    if (n > m)
+        return;
+    System.Console.Write(n + " ");
+    Sum(n + 1, m);
+}
+
+Console.Write("Enter first number: ");
+int firstNumber = int.Parse(Console.ReadLine());
+Console.Write("Enter second number: ");
+int secondNumber = int.Parse(Console.ReadLine());
+Sum(firstNumber, secondNumber);
