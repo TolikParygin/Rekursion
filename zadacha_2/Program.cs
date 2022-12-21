2e5d320ced69b1ef352bba427a5ede847e0175fa
Tolik <anatolijparygin988@gmail.com>
2022-12-21 23:19:20 +0300

    сумма цифр числа

---
+++ zadacha_2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+System.Console.Write("Веедите число N: ");
+int number = int.Parse(Console.ReadLine());
+
+ int Sum(int num)
+{
+    if (num < 10) 
+    return num;
+    else 
+  return num % 10 + Sum(num / 10);
+    
+}
+int sum = Sum(number);
+System.Console.WriteLine(sum);
