02a98b799b88bfe3ee02054d6617937d3a0d9a94
Tolik <anatolijparygin988@gmail.com>
2022-12-22 01:21:28 +0300

    вывод чисел от N до 1

--- zadacha_3/Program.cs
+++ zadacha_3/Program.cs
@@ -1,15 +1,14 @@
-﻿// сумма цифр от N до M
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
-void Sum(int n, int m)
+void EnterNumber(int n)
 {
-    if (n > m)
-        return;
+    if (n == 0) return;
     System.Console.Write(n + " ");
-    Sum(n + 1, m);
+    EnterNumber(n - 1);
 }
 
-Console.Write("Enter first number: ");
-int firstNumber = int.Parse(Console.ReadLine());
-Console.Write("Enter second number: ");
-int secondNumber = int.Parse(Console.ReadLine());
-Sum(firstNumber, secondNumber);
+Console.Write("Enter number: ");
+int number = int.Parse(Console.ReadLine());
+EnterNumber(number);
